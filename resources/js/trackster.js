d1691a3c186aa8811aad153897f55ce779e25af2
Uwe <uwesch62@gmail.com>
2018-02-26 22:36:51 -0800

    Add api call to last.fm.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,25 @@
+/* jQuery */
+
+$(document).ready(function() {
+
+  $("#cmdSearch").click(function() {
+    Trackster.searchTracksByTitle($("#searchText").val());
+  })
+
+});
+
+const API_KEY = "2df2103dfbff06449dce13e12a56ac4a";
+
+
+
+
+/* Trackster Object */
+
 var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
@@ -13,5 +30,11 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+    $.ajax({
+      url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+      datatype: 'jsonp',
+      success: function(data) {
+        console.log("data", data);
+      }
+    });
 };
