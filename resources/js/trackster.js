0f35768ba2d24ec9c0a0b23356e84f19eca56784
Uwe <uwesch62@gmail.com>
2018-02-28 23:13:54 -0800

    Add dynamic list of titles into table body after search.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -6,6 +6,16 @@ $(document).ready(function() {
     Trackster.searchTracksByTitle($("#searchText").val());
   })
 
+  $("#play").click(function() {
+    console.log($("#video").is(":visible"));
+
+    if($("#video").is(":visible")) {
+      $("#video").hide(500);
+    } else {
+      $("#video").show(500);
+    }
+  })
+
 });
 
 const API_KEY = "2df2103dfbff06449dce13e12a56ac4a";
@@ -19,7 +29,20 @@ var Trackster = {};
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  console.log("tracks.results.trackmatches.track", tracks.results.trackmatches.track);
+  $.each(tracks.results.trackmatches.track, function( index, value ) {
+    console.log( index + ": " + value.name );
 
+    const thisRow = '<div id="tableBodyRow_' + index + '" class="row"><div id="tableBody" class="container-fluid"><div class="col col-md-1"></div>' +
+      '<div class="col col-md-1"><i id="play_' + index + '" class="fa fa-play-circle-o fa-2x"></i></div>' +
+      '<div class="col col-md-3">' + value.name + '</div>' +
+      '<div class="col col-md-3">' + value.artist + '</div>' +
+      '<div class="col col-md-1">Some Image</div>' +
+      '<div class="col col-md-1">' + value.listeners + '</div>' +
+      '<div class="col col-md-1"></div>' +
+      '</div></div>';
+    $("#musicTable").append(thisRow);
+  });
 };
 
 /*
@@ -31,7 +54,7 @@ Trackster.searchTracksByTitle = function(title) {
       url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
       datatype: 'jsonp',
       success: function(data) {
-        console.log("data", data);
+        Trackster.renderTracks(data);
       }
     });
 };
