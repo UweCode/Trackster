e3f2fef1f1be4bb5c99ef53047eb153ce468c9dd
Uwe <uwesch62@gmail.com>
2018-03-01 22:56:21 -0800

    Finish basic site: open links on Play-click, populate small images in table, clear/select input, format numbers, search on enter, animate Trackster on search.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -3,18 +3,41 @@
 $(document).ready(function() {
 
   $("#cmdSearch").click(function() {
-    Trackster.searchTracksByTitle($("#searchText").val());
+    const searchText = $("#searchText").val();
+
+    if(searchText.length > 0 && searchText != "Watcha lookin' for?") {
+      $('h1').css('color', 'rgb(255, 255, 255)');
+      setTimeout(function(){ $('h1').css('color', 'rgb(255, 0, 171)'); }, 500);
+
+      Trackster.searchTracksByTitle($("#searchText").val());
+    }
   })
 
-  $("#play").click(function() {
-    console.log($("#video").is(":visible"));
+    $("#searchText").click(function() {
+      if ($("#searchText").val() == "Watcha lookin' for?") {
+        $("#searchText").val("").select();
+      } else {
+        $("#searchText").selectAll();
+      }
+    })
 
+  $("#play").click(function() {
     if($("#video").is(":visible")) {
       $("#video").hide(500);
     } else {
       $("#video").show(500);
     }
   })
+
+  $("#cmdClose").click(function() {
+    $("#searchText").val("").select();
+  })
+
+  $('#searchText').keypress(function(event){
+    if(event.keyCode == 13){
+      $('#cmdSearch').click();
+    }
+  });
 
 });
 
@@ -29,16 +52,17 @@ var Trackster = {};
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
-  console.log("tracks.results.trackmatches.track", tracks.results.trackmatches.track);
+  $("#musicTable").empty();
+
   $.each(tracks.results.trackmatches.track, function( index, value ) {
-    console.log( index + ": " + value.name );
+    var mediumAlbumArt = value.image[0]["#text"]; // [0] - small - [1] - medium - [2] - large - [3] - extra large\
 
     const thisRow = '<div id="tableBodyRow_' + index + '" class="row"><div id="tableBody" class="container-fluid"><div class="col col-md-1"></div>' +
-      '<div class="col col-md-1"><i id="play_' + index + '" class="fa fa-play-circle-o fa-2x"></i></div>' +
+      '<div class="col col-md-1"><a href="' + value.url + '" target="_blank_"><i id="play_' + index + '" class="fa fa-play-circle-o fa-2x"></i></a></div>' +
       '<div class="col col-md-3">' + value.name + '</div>' +
       '<div class="col col-md-3">' + value.artist + '</div>' +
-      '<div class="col col-md-1">Some Image</div>' +
-      '<div class="col col-md-1">' + value.listeners + '</div>' +
+      '<div class="col col-md-1"><img src="' + mediumAlbumArt + '" alt="Album art for the song ' + value.name + ' by ' + value.artist + '."></div>' +
+      '<div class="col col-md-1">' + numeral(value.listeners).format('0,0') + '</div>' +
       '<div class="col col-md-1"></div>' +
       '</div></div>';
     $("#musicTable").append(thisRow);
@@ -51,7 +75,7 @@ Trackster.renderTracks = function(tracks) {
 */
 Trackster.searchTracksByTitle = function(title) {
     $.ajax({
-      url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+      url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
       datatype: 'jsonp',
       success: function(data) {
         Trackster.renderTracks(data);
